3841daa2897f5974ab08338fb9699e45add874b2
MrFantaz <dima1_dima2@mail.ru>
2018-08-07 20:01:33 +0300

    fix and create heroku

---
+++ Tree/src/main/java/com/heroku/servlet/TestServlet.java
@@ -0,0 +1,17 @@
+package com.heroku.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+import java.io.Writer;
+@WebServlet("/index")
+public class TestServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+     doGet(request,response);
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        Writer writer = response.getWriter();
+        writer.append("<h1>PAMPAM</h1>");
+//       request.getRequestDispatcher("index.jsp");
+    }
+}
